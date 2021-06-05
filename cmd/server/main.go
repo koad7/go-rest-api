2cd2a3ba376dd43c70983a63577a4c4ee6e75ce1
Kodjo Adanledji <kad@maclt99.home>
2021-06-06 00:19:52 +0200

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-// App - teh struct 
+	transportHTTP "github.com/koad7/go-rest-api/internal/transport/http"
+)
+
+// App - teh struct
 type App struct{}
 
-// Run - sets up the  application 
+// Run - sets up the  application
 func (app *App) Run() error {
 	fmt.Println("Setting up our API")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up a server")
+		return nil
+	}
 	return nil
 }
 func main() {
